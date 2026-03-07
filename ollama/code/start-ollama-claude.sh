8f68868de3ccb9f4e81d885dfd0b097538db7d3b
shorelskyi <serhii.horelskyi@gmail.com>
2026-03-07 19:54:56 +0100

    Ollama: Use GPT-OSS:20B by default for code and add start-ollama-claude.sh

---
+++ ollama/code/start-ollama-claude.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cp -vf env-file.template .env
+
+echo "Updating .env file..."
+sed -e "s|/home/dockeruser|${HOME}|g" -i .env
+
+export $(grep -v '^#' .env | xargs)
+
+
+echo "Launching OLLAMA AI..."
+echo "OLLAMA MODEL: ${OLLAMA_LLM_MODEL}"
+echo "OLLAMA DATA PATH: ${OLLAMA_DATA}"
+
+
+if [[ ! -d ${OLLAMA_DATA} ]]; then
+    echo "OLLAMA data path ${OLLAMA_DATA} not found. Creating..."
+    mkdir -p ${OLLAMA_DATA}
+fi
+
+echo "Starting service on http://${HOST_IP}:${HOST_PORT}..."
+
+docker rm ollama-llm-code || true
+docker rm ollama-claude-code  || true
+
+docker compose -f docker-compose-claude.yml up
